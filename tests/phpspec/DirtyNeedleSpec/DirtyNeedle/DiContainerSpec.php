10d5cd321f2131c30d52c59062fcc9fa758e3bd2
Sam-Burns <sam.burns.uk@gmail.com>
2015-03-15 01:08:51 +0000

    Improvement to PHPSpec container instantiation

--- tests/phpspec/DirtyNeedleSpec/DirtyNeedle/DiContainerSpec.php
+++ tests/phpspec/DirtyNeedleSpec/DirtyNeedle/DiContainerSpec.php
@@ -7,6 +7,11 @@ use Prophecy\Argument;
 
 class DiContainerSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedThrough('getInstance', array());
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('DirtyNeedle\DiContainer');
