4a6171f0be5c44d59ce630b34fff064a52ebaf1c
Sam-Burns <sam.burns.uk@gmail.com>
2015-03-15 00:40:31 +0000

    Making Blackfire use the same fixtures as other tests

--- tests/blackfire/FixtureClasses/SimpleClass.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace DirtyNeedle\BlackfireTest\FixtureClasses;
-
-class SimpleClass
-{
-}
--- tests/blackfire/benchmarkable_script.php
+++ tests/blackfire/benchmarkable_script.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
 
 $diContainer = \DirtyNeedle\DiContainer::getInstance();
-$diContainer->addConfigFile(__DIR__ . '/fixture_config_files/di.php');
+$diContainer->addConfigFile(DIRTYNEEDLE_TEST_DIR . '/fixtures/config_files/sample_di_config.php');
 
-$dependency = $diContainer->get('dependency-to-retrieve');
+$dependency = $diContainer->get('class-with-one-dependency');
